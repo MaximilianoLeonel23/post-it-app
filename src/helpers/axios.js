b09051e0ffb55ef042a5cdb81445b6808a889548
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-06-24 11:33:26 -0300

    Probando cambios en la ruta a la api

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://localhost:4000/api",
-  baseURL: "https://post-it-app.up.railway.app/api",
+  baseURL: "https://post-it-app.up.railway.app",
   withCredentials: true,
 });
 
