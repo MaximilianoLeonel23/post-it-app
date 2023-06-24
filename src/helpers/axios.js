db3a89fa33500c7d7ad709428ca7f7e2f36ffbed
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-06-24 11:13:33 -0300

    Agrego cambios a la url

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api",
-  // baseURL: "https://post-it-app.up.railway.app/api",
+  // baseURL: "http://localhost:4000/api",
+  baseURL: "https://post-it-app.up.railway.app/api",
   withCredentials: true,
 });
 
