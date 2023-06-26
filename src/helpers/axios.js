383c90191e9e4f30bcfdc4bd01903f5442e7b264
Maximiliano Pérez <maximiliano.leonel.perez1999@gmail.com>
2023-06-26 18:37:01 -0300

    Cambios en la url

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:4000/api",
-  // baseURL: "https://post-it-backend-kappa.vercel.app/api",
+  // baseURL: "http://localhost:4000/api",
+  baseURL: "https://post-it-backend-kappa.vercel.app/api",
   withCredentials: true,
 });
 
